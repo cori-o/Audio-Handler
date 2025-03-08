89ad9937fc3bdbd9dfdfba9ff8dfce31cd446cb8
cori-o <ojd9512@gmail.com>
2025-03-09 02:01:37 +0900

    update vadp

--- src/pyannote-pipe.py
+++ src/pyannote-pipe.py
@@ -14,30 +14,32 @@ class PyannotePipe:
         self.pipeline = Pipeline(model_name, use_auth_token=self.args['hf_key'])
         self.pipeline.to(self.device)
 
-    @abstractmethod 
-    def set_pipe_config(self, **kwargs):
-        pass 
 
 class PyannoteVADP(PyannotePipe):
     def __init__(self, args):
         super().__init__(args)
-    
-    def set_pipe_config(self, pipeline, **kwargs):
-        '''
-        onset: 감지 민감도 증가 
-        offset: 감지 지속시간 조정 
-        '''
-        pipeline.instantiate(kwargs)
-        return pipeline
-    
+
     def get_vad(self, pipeline, audio_file, onset=0.5, offset=0.5, min_duration_on=0.5, min_duration_off=0.5):
         '''
         audio_file = "data_path/tets.wav"
         '''
-        pipeline = self.set_pipe_config(pipeline, onset, offset, min_duration_on, min_duration_off)
+        hyper_params = {
+            "onset": onset, 
+            "offset": offset, 
+            "min_duration_on": min_duration_on,
+            "min_duration_off": min_duration_off
+        }
+        pipeline.instantiate(hyper_params)
         output = pipeline(audio_file)
 
         vad_timestamp = [] 
         for speech in output.get_timeline().support():
             vad_timestamp.append((speech.start, speech.end))
         return vad_timestamp
+    
+
+class PyannoteDIAR(PyannotePipe):
+    def __init__(self, args):
+        super().__init__(args)
+    
+    def get_diar()
