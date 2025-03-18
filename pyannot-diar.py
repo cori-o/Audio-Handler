ba39ee846f8b1eac7cb46eee828d3e693b0972f2
Finger-cori <ojd9512@gmail.com>
2025-03-18 09:46:23 +0900

    Update pyannot-diar.py

--- pyannot-diar.py
+++ pyannot-diar.py
@@ -1,30 +1,30 @@
-from pyannote.audio import Pipeline
+from src import PyannoteDIARP
 from dotenv import load_dotenv
 import pandas as pd
+import argparse
 import torch
 import time
 import json
 import os 
 
-load_dotenv()
-hf_api_key = os.getenv('HF_API')
 
-start = time.time()
-pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-3.1", use_auth_token=hf_api_key)
-start = time.time()
+def main(args):
+    load_dotenv()
+    hf_api_key = os.getenv('HF_API')
+    with open(os.path.join(args.config_path, args.config_file)) as f:
+        audio_config = json.load(f)
+    audio_config['hf_key'] = hf_api_key
+    
+    start = time.time()
+    diar_pipe = PyannoteDIARP(audio_config)
+    diar_result = diar_pipe.get_diar(args.audio_file)
+    df = pd.DataFrame(diar_result, columns=["timestamp", "speaker"])
+    df.to_csv("./dataset/diarization_20250220.csv", index=False)
 
-pipeline.to(torch.device("cuda"))
-
-output = pipeline("./meeting_records/20250220.wav")
-
-diar_result = [] 
-for segment, _, speaker in output.itertracks(yield_label=True):
-    start_time = segment.start 
-    end_time = segment.end
-    duration = end_time - start_time 
-
-    if duration >= 0.7:
-        diar_result.append([(start_time, end_time), speaker])
-
-df = pd.DataFrame(diar_result, columns=["timestamp", "speaker"])
-df.to_csv("./meeting_records/diarization_20250220.csv", index=False)
+if __name__ == '__main__':
+    cli_parser = argparse.ArgumentParser()
+    cli_parser.add_argument('--config_path', type=str, default='./config')
+    cli_parser.add_argument('--config_file', type=str, default='audio_config.json')
+    cli_parser.add_argument('--audio_file', type=str, required=True)
+    cli_args = cli_parser.parse_args()
+    main(cli_args)
