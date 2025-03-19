1c80580375c1511a38721961a341e23a8a71cedb
Finger-cori <ojd9512@gmail.com>
2025-03-19 12:41:39 +0900

    Update get_vad.py
    voice activity detect without network

--- get_vad.py
+++ get_vad.py
@@ -1,27 +1,22 @@
-from pyannote.audio import Inference, Model
-from dotenv import load_dotenv
 from src import PyannotVAD
+from pyannote.audio import Pipeline
+from pathlib import Path
 import argparse
 import torch
-import json
 import os 
 
-def main(args): 
-    load_dotenv()
-    hf_api_key = os.getenv('HF_API')
-    with open(os.path.join(args.config_path, args.config_file)) as f:
-        audio_config = json.load(f)
 
-    audio_config['hf_key'] = hf_api_key
-    pyannot_emb = PyannotVAD(audio_config)
-    pyannot_emb.set_inference()
-    vad = pyannot_emb.get_vad(pyannot_emb.inference, audio_file=args.audio_file)
-    
+def main(args):
+    pyannot_vad = PyannotVAD()
+    pipeline = pyannot_vad.load_pipeline_from_pretrained(os.path.join(args.model_config_path, args.model_config_file))  
+    print(pipeline)
+    vad_result = pyannot_vad.get_vad_timestamp(pipeline, args.audio_file)
+    print(vad_result)
 
 if __name__ == '__main__':
     cli_parser = argparse.ArgumentParser()
-    cli_parser.add_argument('--config_path', type=str, default='config')
-    cli_parser.add_argument('--config_file', type=str, default='audio_config.json')
+    cli_parser.add_argument('--model_config_path', type=str, default='./models')
+    cli_parser.add_argument('--model_config_file', type=str, default='pyannote_vad_config.yaml')
     cli_parser.add_argument('--audio_file', type=str, required=True)
     cli_args = cli_parser.parse_args()
     main(cli_args)
