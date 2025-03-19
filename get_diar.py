37a231f110b59f79a2fa85b592be5dcfdbac758a
Finger-cori <ojd9512@gmail.com>
2025-03-19 11:16:16 +0900

    Update get_diar.py

--- get_diar.py
+++ get_diar.py
@@ -1,31 +1,22 @@
-from pyannote.audio import Inference, Model
-from dotenv import load_dotenv
-from src import PyannotEMB
+from src import PyannotDIAR
+from pyannote.audio import Pipeline
+from pathlib import Path
 import argparse
 import torch
-import json
 import os 
 
-def main(args): 
-    load_dotenv()
-    hf_api_key = os.getenv('HF_API')
-    with open(os.path.join(args.config_path, args.config_file)) as f:
-        audio_config = json.load(f)
 
-    audio_config['hf_key'] = hf_api_key
-    pyannot_emb = PyannotEMB(audio_config)
-    pyannot_emb.set_inference(window='whole')
-    embeddings = pyannot_emb.get_embedding(pyannot_emb.inference, audio_file=args.audio_file)
-    embedding_array = embeddings.data
-
-    print(embeddings)
-    print(embedding_array.shape)
-    print(embedding_array[:3])
+def main(args):
+    pyannot_diar = PyannotDIAR()
+    pipeline = pyannot_diar.load_pipeline_from_pretrained(os.path.join(args.model_config_path, args.model_config_file))  
+    print(pipeline)
+    diar_result = pyannot_diar.get_diar_result(pipeline, args.audio_file)
+    print(diar_result)
 
 if __name__ == '__main__':
     cli_parser = argparse.ArgumentParser()
-    cli_parser.add_argument('--config_path', type=str, default='config')
-    cli_parser.add_argument('--config_file', type=str, default='audio_config.json')
+    cli_parser.add_argument('--model_config_path', type=str, default='./models')
+    cli_parser.add_argument('--model_config_file', type=str, default='pyannote_diarization_config.yaml')
     cli_parser.add_argument('--audio_file', type=str, required=True)
     cli_args = cli_parser.parse_args()
     main(cli_args)
