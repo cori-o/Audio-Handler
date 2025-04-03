11beaab478c1d6fdccef4b6b3a6a62d62afab3ac
Finger-cori <ojd9512@gmail.com>
2025-04-03 16:02:36 +0900

    Update denoise_audio.py
    add model_type

--- denoise_audio.py
+++ denoise_audio.py
@@ -4,10 +4,11 @@ import os
 
 def main(args):
     noise_handler = NoiseHandler()
-    noise_handler.denoise_audio(args.file_name, data_path='./dataset/')
+    noise_handler.denoise_audio(args.file_name, model_type=args.model_type, data_path='./dataset/')
 
 if __name__ == '__main__':
     cli_parser = argparse.ArgumentParser()
     cli_parser.add_argument('--file_name', type=str, required=True)
+    cli_parser.add_argument('--model_type', type=str, default='nsnet')
     cli_args = cli_parser.parse_args()
     main(cli_args)
