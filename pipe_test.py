dc82bb988c045302602f4f08c5d7a65fcbdb1116
Finger-cori <ojd9512@gmail.com>
2025-04-07 15:13:44 +0900

    FrontendProcessor test code
    front-en

---
+++ pipe_test.py
@@ -0,0 +1,16 @@
+from src import FrontendProcessor
+import argparse 
+import os 
+
+def main(args):
+    frontend_processor = FrontendProcessor()
+    frontend_processor.set_env()
+    clean_audio = frontend_processor.process_audio(args.file_name)
+    frontend_processor.save_audio(clean_audio, './dataset/denoised/pipe-test.wav')
+
+
+if __name__ == '__main__':
+    cli_parser = argparse.ArgumentParser()
+    cli_parser.add_argument('--file_name', type=str, required=True)
+    cli_args = cli_parser.parse_args()
+    main(cli_args)
