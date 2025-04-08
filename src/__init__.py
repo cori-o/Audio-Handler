c3bfe967459c0746636e71d4665b416ef51f2aec
Finger-cori <ojd9512@gmail.com>
2025-04-08 10:46:36 +0900

    Update __init__.py
    add pipe classes

--- src/__init__.py
+++ src/__init__.py
@@ -2,3 +2,4 @@ from .preprocessors import DataProcessor, AudioFileProcessor
 from .audio_handler import VoiceEnhancer, NoiseHandler, AudioVisualizer
 from .pyannotes import PyannotVAD, PyannotDIAR, PyannotOSD
 from .speechbrains import SBEMB
+from .pipe import FrontendProcessor, PostProcessor
